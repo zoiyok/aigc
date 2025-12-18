578f11b5d17c7de120eb9ab1e73191c52fbc6db7
zane <zoiyok@outlook.com>
2025-12-18 19:17:22 +0800

    第一次分支测试

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCh0arm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/this is a developed version
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/this is a developed version99999
